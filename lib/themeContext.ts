5d3c90894e9575d138cf907e9a7217963535a093
mayudev <99561108+mayudev@users.noreply.github.com>
2022-04-18 20:00:45 +0200

    fix: set theme from localStorage

--- lib/themeContext.ts
+++ lib/themeContext.ts
@@ -2,6 +2,13 @@ import React, { createContext } from "react";
 
 export type ThemeMode = "dark" | "bluishDark" | "light";
 
+// for type checking
+const themeModes = ["dark", "bluishDark", "light"];
+
 export const ThemeContext = createContext<
   [ThemeMode, React.Dispatch<React.SetStateAction<ThemeMode>>]
 >(["dark", () => {}]);
+
+export function isTheme(value: string): value is ThemeMode {
+  return themeModes.indexOf(value) >= 0;
+}
