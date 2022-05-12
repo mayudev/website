7969303db969fb11bbefc3e6a1f8fc5bdef498c0
mayudev <99561108+mayudev@users.noreply.github.com>
2022-05-12 18:44:40 +0200

    feat(tags): add styling to tag index

--- components/post/post-tags.tsx
+++ components/post/post-tags.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { PropsWithChildren } from "react";
+import React from "react";
 import styled from "styled-components";
 import { ForegroundSecondary } from "../../lib/themes";
 
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,15 @@
+import { faNoteSticky } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 import styled from "styled-components";
+import NiceLink, { NiceLinkText } from "../../components/home/nice-link";
 import Layout from "../../components/layout";
 import PostSummary from "../../components/post-summary";
 import Tags from "../../components/post/post-tags";
 import { getSortedPosts, PostData } from "../../lib/posts";
 import { getUniqueTags } from "../../lib/tags";
-import { Accent } from "../../lib/themes";
+import { ForegroundSecondary } from "../../lib/themes";
 
 type Props = {
   posts: PostData[];
@@ -14,15 +18,27 @@ type Props = {
 };
 
 const TagHeading = styled.h1`
-  font-size: 1.5rem;
+  font-size: 1.8rem;
+  padding-left: 1rem;
 
-  color: ${Accent};
+  color: ${ForegroundSecondary};
+  border-left: 3px solid ${ForegroundSecondary};
 `;
 
 export default function BlogPosts({ posts, tag, tags }: Props) {
   return (
     <Layout post>
-      {tag && <TagHeading>tag: {tag}</TagHeading>}
+      {tag && (
+        <>
+          <Link href="/posts" passHref>
+            <NiceLink>
+              <FontAwesomeIcon icon={faNoteSticky} />
+              <NiceLinkText>See all posts</NiceLinkText>
+            </NiceLink>
+          </Link>
+          <TagHeading>tag: {tag}</TagHeading>
+        </>
+      )}
       {posts.map((post) => (
         <PostSummary key={post.slug} post={post} />
       ))}
